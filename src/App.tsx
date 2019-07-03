2b2ce9432e3249b68c9ccc843b740e1450196886
HanRui <597589373@qq.com>
2019-07-03 14:51:29 +0800

    1

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+ 
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        55555555555555555
+      </div>
+    );
+  }
+}
+
+export default App;
