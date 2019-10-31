2688d242ef5233a4faca4fb79eaf7307f4fc16ab
HanRui <597589373@qq.com>
2019-10-31 17:16:11 +0800

    1

--- src/routers/login.tsx
+++ src/routers/login.tsx
@@ -1,11 +1,20 @@
-import React, { Component } from "react";
+import * as React from "react";
 import { LoginForm } from "../components";
-
-export class SMMSLogin extends Component {
+import { Card, Layout, Row, Col } from "antd";
+import * as Style from "./style/login.module.less";
+export class SMMSLogin extends React.Component {
   render() {
     return (
       <div>
-        <LoginForm />
+        <Layout className={Style.Layout}>
+          <Row type="flex" justify="center" align="top">
+            <Col>
+              <Card title="登录">
+                <LoginForm />
+              </Card>
+            </Col>
+          </Row>
+        </Layout>
       </div>
     );
   }
