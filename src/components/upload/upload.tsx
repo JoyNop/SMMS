2a0bac32f9c05a75568364b6133d4016ffca2bed
HanRui <597589373@qq.com>
2019-11-05 16:22:57 +0800

    1

--- src/components/upload/upload.tsx
+++ src/components/upload/upload.tsx
@@ -1,18 +1,21 @@
 import * as React from "react";
-import { Upload, Icon, message } from "antd";
+import { Upload, Icon, message, Modal, Button, Alert, Input } from "antd";
 import _ from "lodash";
 import axios from "axios";
 import { RcUpload, CustomRequest, SMMS_V2_PicReq } from "../../interfaces";
 import { SMMSAPI } from "../../config";
 import * as Style from "./upload.module.less";
 import { UploadFile } from "antd/lib/upload/interface";
-
+import copy from "copy-to-clipboard";
+const Search = Input.Search;
 const { Dragger } = Upload;
 
 export class SMMSUpload extends React.Component {
   private SMMSAPI = new SMMSAPI();
   private fileList: Array<UploadFile> = [];
-
+  private visible: boolean = false;
+  private uploadUrl: string = "";
+  private isCopy: boolean = false;
   private configImage = () => {
     const _this = this;
     let config: CustomRequest = {
@@ -42,6 +45,8 @@ export class SMMSUpload extends React.Component {
         if (ret.success) {
           _this.fileList[_this.fileList.length - 1].url = ret.data.url;
           _this.fileList[_this.fileList.length - 1].status = "success";
+          _this.uploadUrl = ret.data.url;
+          _this.visible = false;
           console.log("onSuccess");
           _this.setState({});
         } else {
@@ -129,9 +134,67 @@ export class SMMSUpload extends React.Component {
             5 MB max per file. 1[0] files max per request.
           </p>
         </Dragger>
+        <Modal
+          title="系统消息:上传成功!"
+          visible={this.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          footer={
+            [
+              // <Button key="back" onClick={this.handleCancel}>
+              //   Return
+              // </Button>,
+              // <Button key="submit" type="primary"   onClick={this.handleOk}>
+              //   Submit
+              // </Button>,
+            ]
+          }
+        >
+          {this.isCopy ? (
+            <Alert
+              message="成功复制到粘贴板!"
+              description="现在您可以将链接粘贴到其他地方:)"
+              type="success"
+              showIcon
+            />
+          ) : (
+            <Alert
+              message="您可以在此处复制该图片链接"
+              description="点击[复制]按钮,一键复制到粘贴板."
+              type="info"
+              showIcon
+            />
+          )}
+
+          <Search
+            style={{ marginTop: "20px" }}
+            enterButton={<Icon type="copy" />}
+            size="large"
+            defaultValue={this.uploadUrl}
+            onSearch={value => this.copyUrl(value)}
+          />
+        </Modal>
       </div>
     );
   }
 
+  private copyUrl = (url: string) => {
+    copy(this.uploadUrl, {
+      debug: true,
+      message: "Press #{key} to copy"
+    });
+    this.isCopy = true;
+    this.setState({});
+  };
+
+  private handleOk = () => {
+    this.visible = false;
+    this.setState({});
+  };
+
+  private handleCancel = () => {
+    this.visible = !this.visible;
+    this.setState({});
+  };
   componentDidMount() {}
 }
